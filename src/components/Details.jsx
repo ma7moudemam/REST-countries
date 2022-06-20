e4d5bcb09b31d73bde2964998067644f6ba04119
m7moud emam <m7moud_emam@hotmail.com>
2022-06-20 09:34:20 +0200

    finish Details

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -37,10 +37,26 @@ const Details = () => {
               </div>
             <div className={classes.content}>
               <h1>{country.name.common}</h1>
+              <div className={classes.innerContent}>
               <p>Native Name: <span>{country.name?.nativeName ?Object.values(country.name?.nativeName)[0].common : country.name?.common}</span></p>
               <p>Population: <span>{country.population}</span></p>
               <p>Region: <span>{country.region}</span></p>
               <p>Capital: <span>{country.capital}</span></p>
+              <p>Sub Region: <span>{country.subregion}</span></p> 
+              <p>Top Level Domain: <span>{country.tld}</span></p>
+              <p>Currencies: <span>{ country.currencies && Object.values(country.currencies)[0].name}</span></p>
+              <p>Languages: <span>{country.languages && Object.values(country.languages).join(', ')}</span></p>
+              </div>
+              <div className={classes.border}>
+                <p>Border Countries: </p>
+                <ul>
+                  {country.borders.map((border) => {
+                    return  <Link className={classes.couBorder} to={`/`} key={border}>{border}</Link>
+                  }
+                  )}
+                </ul>
+              </div>
+             
             </div>
           </div>
         );
